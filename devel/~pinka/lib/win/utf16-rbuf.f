9f05f8008c3b6eb9720e76f58383f96ef857d1ad
ruv <ruv@users.noreply.github.com>
2023-12-11 15:39:14 +0400

    UPD devel ~ lib win utf16-rbuf -- correction and improvement
    - do not include the null-termial character into the size of the output string;
    - check that the input string is not too long;
    - "ENSURE-ASCIIZ-R" is not needed since "UTF8>UTF16" does not require the input string
      to be null-terminated;

--- devel/~pinka/lib/win/utf16-rbuf.f
+++ devel/~pinka/lib/win/utf16-rbuf.f
@@ -1,8 +1,16 @@
-REQUIRE ENSURE-ASCIIZ-R         ~pinka/spf/rbuf.f
+REQUIRE RBUF                    ~pinka/spf/rbuf.f
 REQUIRE UTF8>UTF16              ~pinka/lib/win/utf16.f
+REQUIRE [:                      lib/include/quotation.f
 
 : R:UTF8>UTF16 \ Run-time ( sd.txt1 -- sd.txt2 )
-  \ sd.txt2 is allocated on the return stack
-  POSTPONE ENSURE-ASCIIZ-R POSTPONE 1+ POSTPONE DUP POSTPONE 2* POSTPONE RBUF POSTPONE UTF8>UTF16
+  \ Only for compilation.
+  \ The max size of the input string sd.txt1 is 4 KiB (otherwise, -12 "argument type mismatch" is thrown).
+  \ sd.txt2 is null-terminated, and its size does not include this character (2 bytes).
+  \ sd.txt2 is allocated on the return stack and is automatically freed when the containing definition returns.
+  \ Local variables in the containing definition (if any) become inaccessible.
+  [: DUP 4096 U> IF -12 THROW THEN  DUP 1+ 2* ;] COMPILE,
+  \ NB: in the worst case utf16 takes 2x of utf8
+  POSTPONE RBUF
+  [: UTF8>UTF16  2DUP + 0 SWAP W! ;] COMPILE,
 ; IMMEDIATE
 
